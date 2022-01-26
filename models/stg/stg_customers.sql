98302cde814abf5184ff7f2a0d7f3101df3f17bb
Amit Sharma <amitsharma@Admins-MacBook-Pro.local>
2022-01-27 00:11:19 +0530

    new machine setup

--- models/stg/stg_customers.sql
+++ models/stg/stg_customers.sql
@@ -2,4 +2,4 @@ select
     id as customer_id,
     first_name,
     last_name
-from dbt.jaffle_shop.customers
+from dbt_db.jaffle_shop.customers
--- models/stg/stg_orders.sql
+++ models/stg/stg_orders.sql
@@ -3,4 +3,4 @@ select
     user_id as customer_id,
     order_date,
     status
-from dbt.jaffle_shop.orders
+from dbt_db.jaffle_shop.orders
